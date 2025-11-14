538fd3c0e7622e037a5de9b0f7cc47d4bf9931af
nickolashkraus <0x@nickolaskraus.io>
2025-11-14 09:54:48 -0600

    Add app/ and tests/

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+"""A custom authorization implementation that conforms to the Model Context
+Protocol (MCP) authorization specification.
+
+See: https://modelcontextprotocol.io/specification/2025-06-18/basic/authorization
+"""
