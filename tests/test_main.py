398f4f4ba6adce909b90576eb761d034beaca7e8
nickolashkraus <0x@nickolaskraus.io>
2025-11-18 14:28:44 -0600

    Add tests for application entrypoint

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,10 @@
 """Tests for application entrypoint."""
+
+from fastapi.testclient import TestClient
+
+
+def test_root_endpoint(client: TestClient):
+    """Test root endpoint returns message."""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello, World!"}
