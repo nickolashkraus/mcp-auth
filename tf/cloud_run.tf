6249a33afd02fdf09599766731fd440292c0bcef
nickolashkraus <0x@nickolaskraus.io>
2025-11-19 15:10:24 -0600

    Allow public access via IAM

--- tf/cloud_run.tf
+++ tf/cloud_run.tf
@@ -17,6 +17,12 @@ resource "google_cloud_run_v2_service" "app" {
         }
       }
     }
+
+    scaling {
+      min_instance_count = 0
+      max_instance_count = 1
+    }
+
   }
 
   # NOTE: To only allow internal traffic, set to `INGRESS_TRAFFIC_INTERNAL_ONLY`.
@@ -25,4 +31,13 @@ resource "google_cloud_run_v2_service" "app" {
   labels = {
     managed-by = "terraform"
   }
+}
+
+resource "google_cloud_run_v2_service_iam_binding" "app" {
+  name = google_cloud_run_v2_service.app.name
+  role = "roles/run.invoker"
+
+  # NOTE: "allUsers" is a special identifier that represents anyone who is on
+  # the internet; with or without a Google account.
+  members = ["allUsers"]
 }
